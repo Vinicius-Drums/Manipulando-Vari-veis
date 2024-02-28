351d208985aad319cca07d2ec8b92ae4ea67acf8
Vincius-Drums <anaogeraldino@gmail.com>
2024-02-28 15:54:39 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Informações Pessoais em Java
+
+Este é um simples programa Java que declara variáveis para armazenar a primeira letra do nome, idade e altura, seguido pela impressão dessas variáveis na tela.
+
+
+
+## Contribuição
+
+Sinta-se à vontade para contribuir com melhorias, correções de bugs ou adicionar novos recursos. Abra um problema ou envie uma solicitação de pull.
+
---
+++ src/App.java
@@ -0,0 +1,14 @@
+// Declaração da Classe
+public class App {
+    public static void main(String[] args) {
+        // Declaração e atribuição das variáveis
+        char primeiraLetraNome = 'V'; 
+        int idade = 36; 
+        double altura = 1.65; 
+
+        // Impressão das variáveis na tela
+        System.out.println("Primeira letra do nome: " + primeiraLetraNome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " metros");
+    }
+}
